4421ed1cdeaceb4d3483f35bce9c5a0d1f712d0d
Syrrel Cabiling <syrrelcabiling@Syrrels-MBP.fc.ca>
2020-02-10 16:10:57 -0500

    nav, landing, footer

---
+++ EatPage.js
@@ -0,0 +1,8 @@
+export default {
+    template: `
+    <section id="eat">
+        <h1>U DUMB</h1>
+    </section>
+    `
+
+}
--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 import SplashPage from "./modules/SplashPage.js";
 import AppPage from "./modules/AppPage.js";
 import ErrorPage from "./modules/ErrorPage.js";
+import EatPage from "./modules/EatPage.js";
+
 
 
 const router = new VueRouter ({ //This acts like app.get router using node and express
     routes: [
         { path: '/', name: "splash", component: SplashPage }, //SplashPage will be rendered out inside router-view!!!!!!!!!!!!
         { path: '/app', name: "app", component: AppPage }, //AppPage will be rendered out inside router-view!!!!!!!!!!!!
-        { path: '*', name: "error", component: ErrorPage } //* catches anything that doesnt match MAKE THIS THE VERY LAST TO AVOID PROBLEMSe
+        { path: '*', name: "error", component: ErrorPage }, //* catches anything that doesnt match MAKE THIS THE VERY LAST TO AVOID PROBLEMSe
+        { path: '#eat', name: "eat", component: EatPage } //* catches anything that doesnt match MAKE THIS THE VERY LAST TO AVOID PROBLEMSe
 
     ]
 })
