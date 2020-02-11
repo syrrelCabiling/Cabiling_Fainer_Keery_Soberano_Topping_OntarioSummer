8956c22aca443df8185a1bb194c5393f85075fb8
Your Name <n_fainer@fanshaweonline.ca>
2020-02-10 23:58:08 -0500

    built carousel

--- js/modules/SplashPage.js
+++ js/modules/SplashPage.js
@@ -141,6 +141,42 @@ export default { //everything should go inside the section tag
     </div>
 
     </div>
+
+
+    <div class="row carousel-container">
+
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div class="carousel-content">
+              <img class="d-block w-100" src="images/bg.jpg" alt="First slide">
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio aperiam maxime eum inventore, repudiandae harum odit ducimus unde veritatis amet!</p>
+            </div>
+            
+          </div>
+          <div class="carousel-item">
+            <div class="carousel-content">
+              <img class="d-block w-100" src="images/bg.jpg" alt="Second slide">
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio aperiam maxime eum inventore, repudiandae harum odit ducimus unde veritatis amet!</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <div class="carousel-content">
+            <img class="d-block w-100" src="images/bg.jpg" alt="Third slide">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio aperiam maxime eum inventore, repudiandae harum odit ducimus unde veritatis amet!</p>
+          </div>
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </div>
     
         </section>
 
