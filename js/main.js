214ea22322b67d874b171bd002e24d30ddc5523d
Your Name <n_fainer@fanshaweonline.ca>
2020-02-10 21:42:11 -0500

    lets do this

--- js/main.js
+++ js/main.js
@@ -30,3 +30,24 @@ const vm = new Vue ({ //make this first
 
     router //shorthand for router ???????
 }).$mount('#app');
+
+
+
+$('#myCarousel').carousel({
+    interval: 10000
+  })
+  
+  $('.carousel .item').each(function(){
+    var next = $(this).next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+    
+    if (next.next().length>0) {
+      next.next().children(':first-child').clone().appendTo($(this));
+    }
+    else {
+        $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+    }
+  });
